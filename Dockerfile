22828fcfa0165aeaebfe4db8eebcb44bf9101380
yasserisa <yasser.isam@gmail.com>
2017-11-07 17:33:41 -0300

    ADD HTTP LOAD BALANCER

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,10 @@ RUN echo "deb http://deb.debian.org/debian/ buster main contrib non-free" > /etc
 
 COPY config/scripts /scripts/
 
-COPY config/proxy/stream.conf /etc/nginx/stream.conf
+COPY config/proxy/stream.conf /etc/nginx/stream/stream.conf
+COPY config/proxy/default.conf /etc/nginx/conf.d/default.conf
 
-RUN echo "include /etc/nginx/stream.conf;" >> /etc/nginx/nginx.conf \
+RUN echo "include /etc/nginx/stream/stream.conf;" >> /etc/nginx/nginx.conf \
         && rm -rf /var/lib/apt/lists/* \
 # if we have leftovers from building, let's purge them (including extra, unnecessary build deps)
 	&& if [ -n "$tempDir" ]; then \
--- config/scripts/links.py
+++ config/scripts/links.py
@@ -17,6 +17,7 @@ except:
 # append new upstream
 with open("/etc/nginx/upstream.conf.new", "a") as upstream_new:
     upstream_new.write("upstream lb {\n")
+    upstream_new.write("    ip_hash;\n")
     # connect to API Docker
     session = requests_unixsocket.Session()
     # Get network ID
@@ -29,4 +30,27 @@ with open("/etc/nginx/upstream.conf.new", "a") as upstream_new:
     upstream_new.write("}")
 # Set upstream
 copyfile("/etc/nginx/upstream.conf.new", "/etc/nginx/upstream.conf")
+
+# delete old stream
+try:
+    os.remove("/etc/nginx/stream.conf.new")
+except:
+    print "nothing to erase"
+
+# append new stream
+with open("/etc/nginx/stream.conf.new", "a") as stream_new:
+    stream_new.write("upstream lb {\n")
+    # connect to API Docker
+    session = requests_unixsocket.Session()
+    # Get network ID
+    networkID = session.get("http+unix://%2Fvar%2Frun%2Fdocker.sock/containers/" +  str(os.environ["HOSTNAME"])  + "/json").json()["NetworkSettings"]["Networks"].values()[0]["NetworkID"]
+    # Get containers in network
+    containers = session.get("http+unix://%2Fvar%2Frun%2Fdocker.sock/networks/" + networkID).json()["Containers"]
+    for key, value in containers.iteritems():
+        if "_lb_" not in value["Name"]:
+            stream_new.write("    server " + value["IPv4Address"].split("/")[0] + ":" + str(os.environ["PORT_SERVICE"]) + " max_fails=1 fail_timeout=1s;\n")
+    stream_new.write("}")
+# Set stream
+copyfile("/etc/nginx/stream.conf.new", "/etc/nginx/stream.conf")
+
 call(["nginx", "-g", "daemon off;"])
